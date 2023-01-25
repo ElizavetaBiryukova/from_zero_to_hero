dfadc3ccd1d6c1a7f3b347dae157cfc07438e4da
Elizaveta Biryukova <birliza@mail.ru>
2023-01-25 17:29:27 +0200

    Adds slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+    //menu
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.main-nav');
     const navList = document.querySelector('.main-nav__list');
@@ -11,3 +12,41 @@
         nav.classList.remove('main-nav--open');
         burger.classList.remove('burger--open');
     });
+
+    //slider
+    const prevBtn = document.querySelector('.slider__button--prev');
+    const nextBtn = document.querySelector('.slider__button--next');
+    const sliders = document.querySelectorAll('.slider__slide');
+
+    let index = 0;
+
+const activeSlide = n => {
+    sliders.forEach(element => {
+        element.classList.add('visually-hidden');
+    });
+    sliders[n].classList.remove('visually-hidden');
+};
+
+const nextSlide = () => {
+    if (index == sliders.length - 1) {
+        index = 0;
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+};
+
+nextBtn.addEventListener('click', nextSlide);
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = sliders.length - 1;
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+};
+
+prevBtn.addEventListener('click', prevSlide);
