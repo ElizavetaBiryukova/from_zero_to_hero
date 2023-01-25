89901788d9cf84b125980095cfbd9d1c9a118afa
Elizaveta Biryukova <birliza@mail.ru>
2023-01-25 15:57:33 +0200

    Change menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.main-nav');
-const navList = document.querySelector('.main-nav__list');
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    const navList = document.querySelector('.main-nav__list');
 
-burger.addEventListener('click', (event) => {
-    event.preventDefault();
-    nav.classList.toggle('main-nav--open');
-    burger.classList.toggle('burger--open');
-});
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('main-nav--open');
+        burger.classList.toggle('burger--open');
+    });
 
-
-navList.addEventListener('click', () => {
-    nav.classList.toggle('main-nav--open');
-});
+    navList.addEventListener('click', () => {
+        nav.classList.remove('main-nav--open');
+        burger.classList.remove('burger--open');
+    });
